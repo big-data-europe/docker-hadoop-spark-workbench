b0cece39c1fa8c5b83dbce52743e4af16d2aa06b
Ivan Ermilov <earthquakesan@gmail.com>
2016-03-21 23:39:30 +0100

    added hue to the repository

--- buildall.sh
+++ buildall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-for i in hadoop namenode datanode resourcemanager nodemanager historyserver spark spark-master spark-worker spark-notebook; do
+for i in hadoop namenode datanode resourcemanager nodemanager historyserver spark spark-master spark-worker spark-notebook ant huebuildenv hue; do
     ( cd $i && ./build.sh)
 done
