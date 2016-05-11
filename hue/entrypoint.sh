01392ec81b82eca3d3f9ce09a7f7c8a9c33092d1
Ivan Ermilov <earthquakesan@gmail.com>
2016-05-11 14:55:21 +0200

    moved hue nodename host to docker-compose file

--- hue/entrypoint.sh
+++ hue/entrypoint.sh
@@ -3,7 +3,6 @@
 HUE_HOME="/opt/hue/"
 HUE_CONFIG_FILE="$HUE_HOME/desktop/conf/pseudo-distributed.ini"
 APP_BLACKLIST="search,security,oozie,jobbrowser,pig,beeswax,search,zookeeper,impala,rdbms,spark,metastore,hbase,sqoop,jobsub,about"
-NAMENODE_HOST="namenode"
 
 sed -i "s#.*blacklist.*#  app_blacklist=$APP_BLACKLIST#g" $HUE_CONFIG_FILE
 sed -i "s#.*webhdfs_url.*#  webhdfs_url=http://$NAMENODE_HOST:50070/webhdfs/v1#g" $HUE_CONFIG_FILE
